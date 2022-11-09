ae5ced5f10c520f073cd3eb4dfc230b277956cc1
prajeshkasodiya <patelprajesh232@gmail.com>
2022-11-09 10:58:34 +0530

    questio 2

--- myproject/lib/main.dart
+++ myproject/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myproject/Designs/ALODOCKTER.dart';
 import 'package:myproject/Designs/DemoApplicationDesign.dart';
 import 'package:myproject/Designs/Food_Express.dart';
-import 'package:myproject/Module-2/Question_2.dart';
+import 'package:myproject/Module-2/QuestionTwo.dart';
 import 'package:myproject/Shopping_App/Pages/HomePage.dart';
 import 'package:myproject/Designs/flutter_dart.dart';
 import 'package:myproject/Designs/TaskDesign.dart';
@@ -11,7 +11,7 @@ import 'package:myproject/List.dart';
 import 'package:myproject/Shopping_App/widgets/HomeAppbar.dart';
 import 'package:myproject/program1.dart';
 import 'package:myproject/Designs/puzzle.dart';
-import 'package:myproject/Module-2/Question_1.dart';
+import 'package:myproject/Module-2/QuestionOne.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Question_two(),
+      home: QuestionTwo(),
     );
   }
 }
